ddae05f117677a11f2d3b7e61369addd7d9400e9
Emma24968 <emnabs12@gmail.com>
2025-07-05 21:34:57 +0100

    I created a paragraph section to store my score

--- script.js
+++ script.js
@@ -1,74 +1,82 @@
-      let score =JSON.parse(localStorage.getItem('score')); 
-      
-      if(!score){
-        score = {
-        win:0,
-        losses:0,
-        tie:0,
-      }
-      }
+let score = JSON.parse(localStorage.getItem("score"));
 
-      function computerMove() {
-        let computerMove = Math.random();
-        let computerChoice = "";
-        if (computerMove >= 0 && computerMove < 1 / 3) {
-          computerChoice = "Rock";
-        } else if (computerMove >= 0 && computerMove < 2 / 3) {
-          computerChoice = "Paper";
-        } else if (computerMove >= 0 && computerMove < 3 / 3) {
-          computerChoice = "Scissors";
-        }
-        return computerChoice;
-      }
+if (!score) {
+  score = {
+    win: 0,
+    losses: 0,
+    tie: 0,
+  };
+}
+myScore();
 
-      function playerMove(playGame) {
-       const computerChoice = computerMove();
-        let result = "";
-        if (playGame === "Rock") {
-          if (computerChoice === "Rock") {
-            result = "Tie";
-          } else if (computerChoice === "Paper") {
-            result = "Win";
-          } else if (computerChoice === "Scissors") {
-            result = "Lose";
-          }
-        } else if (playGame === "Paper") {
-          if (computerChoice === "Rock") {
-            result = "Lose";
-          } else if (computerChoice === "Paper") {
-            result = "Tie";
-          } else if (computerChoice === "Scissors") {
-            result = "Win";
-          }
-        } else if (playGame === "Scissors") {
-          if (computerChoice === "Rock") {
-            result = "Lose";
-          } else if (computerChoice === "Paper") {
-            result = "Win";
-          } else if (computerChoice === "Scissors") {
-            result = "Tie";
-          }
-        }
+function computerMove() {
+  let computerMove = Math.random();
+  let computerChoice = "";
+  if (computerMove >= 0 && computerMove < 1 / 3) {
+    computerChoice = "Rock";
+  } else if (computerMove >= 0 && computerMove < 2 / 3) {
+    computerChoice = "Paper";
+  } else if (computerMove >= 0 && computerMove < 3 / 3) {
+    computerChoice = "Scissors";
+  }
+  return computerChoice;
+}
 
-        if(result === "Win"){
-          score.win += 1;
-        }else if(result === 'Lose'){
-          score.losses += 1;
-        }else if(result === "Tie"){
-          score.tie += 1;
-        } 
-        
-        localStorage.setItem('score', JSON.stringify(score));
+function playerMove(playGame) {
+  const computerChoice = computerMove();
+  let result = "";
+  if (playGame === "Rock") {
+    if (computerChoice === "Rock") {
+      result = "Tie";
+    } else if (computerChoice === "Paper") {
+      result = "Win";
+    } else if (computerChoice === "Scissors") {
+      result = "Lose";
+    }
+  } else if (playGame === "Paper") {
+    if (computerChoice === "Rock") {
+      result = "Lose";
+    } else if (computerChoice === "Paper") {
+      result = "Tie";
+    } else if (computerChoice === "Scissors") {
+      result = "Win";
+    }
+  } else if (playGame === "Scissors") {
+    if (computerChoice === "Rock") {
+      result = "Lose";
+    } else if (computerChoice === "Paper") {
+      result = "Win";
+    } else if (computerChoice === "Scissors") {
+      result = "Tie";
+    }
+  }
 
-        alert(
-          `You picked ${playGame}. Computer picked ${computerChoice}. Its a ${result} 
+  if (result === "Win") {
+    score.win += 1;
+  } else if (result === "Lose") {
+    score.losses += 1;
+  } else if (result === "Tie") {
+    score.tie += 1;
+  }
+
+  localStorage.setItem("score", JSON.stringify(score));
+  myScore();
+
+  alert(
+    `You picked ${playGame}. Computer picked ${computerChoice}. Its a ${result} 
 Win: ${score.win}, Losses: ${score.losses}, Tie: ${score.tie}`
-        );
-      }
+  );
+}
 
-          function resetScore(){ 
-              score.win = 0;
-            score.losses = 0;
-            score.tie = 0;
-            alert(`Score was reset`)
-          }
+function resetScore() {
+  score.win = 0;
+  score.losses = 0;
+  score.tie = 0;
+  alert(`Score was reset`);
+}
+
+function myScore() {
+  document.querySelector(
+    ".myScore"
+  ).innerHTML = `Win: ${score.win}, Losses: ${score.losses}, Tie: ${score.tie}`;
+}
