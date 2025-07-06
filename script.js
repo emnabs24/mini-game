163eb9212aa88946184dad6ccb0f0015fec9780b
Emma24968 <emnabs12@gmail.com>
2025-07-06 21:30:12 +0100

    I moved my onclick attribute to my scipt file clean up my code

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+document.getElementById("rockBtn").addEventListener("click", () => {
+  playerMove("Rock");
+});
+
+document.getElementById("paperBtn").addEventListener("click", () => {
+  playerMove("Paper");
+});
+
+document.getElementById("scissorsBtn").addEventListener("click", () => {
+  playerMove("Scissors");
+});
+
+document.getElementById("resetBtn").addEventListener("click", () => {
+  localStorage.removeItem("score");
+  resetScore();
+});
+
+
 let score = JSON.parse(localStorage.getItem("score"));
 
 if (!score) {
@@ -49,11 +67,11 @@ function playerMove(playGame) {
     } else if (computerChoice === "Scissors") {
       result = "Tie";
     }
-    document.querySelector(".myResult").innerHTML = `You picked ${playGame}.`;
-    document.querySelector(
-      ".computerResult"
-    ).innerHTML = ` Computer picked ${computerChoice}.`;
   }
+  document.querySelector(".myResult").innerHTML = `You picked ${playGame}.`;
+  document.querySelector(
+    ".computerResult"
+  ).innerHTML = ` Computer picked ${computerChoice}.`;
 
   if (result === "Win") {
     score.win += 1;
@@ -66,17 +84,17 @@ function playerMove(playGame) {
   localStorage.setItem("score", JSON.stringify(score));
   myScore();
 
-  alert(
-    `You picked ${playGame}. Computer picked ${computerChoice}. Its a ${result} 
-Win: ${score.win}, Losses: ${score.losses}, Tie: ${score.tie}`
-  );
+//   alert(
+//     `You picked ${playGame}. Computer picked ${computerChoice}. Its a ${result} 
+// Win: ${score.win}, Losses: ${score.losses}, Tie: ${score.tie}`
+//   );
 }
 
 function resetScore() {
   score.win = 0;
   score.losses = 0;
   score.tie = 0;
-  alert(`Score was reset`);
+  // alert(`Score was reset`);
 }
 
 function myScore() {
